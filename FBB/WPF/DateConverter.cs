48a8a16c292d4a6e16f673d2a8378a11f93af3dd
Peter Tran <Peter_Tran@sluz.ch>
2020-12-17 13:57:18 +0100

    Added: DateConverter, advanced date search MainWIndow GridSplitters Modified: MainWindow

---
+++ FBB/WPF/DateConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FBB.WPF
+{
+    public class DateConverter : IValueConverter
+    {
+        private DateTime timePickerDate;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            timePickerDate = ((DateTime)(value));
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return timePickerDate;
+
+            var datePickerDate = ((DateTime)(value));
+
+            if (datePickerDate.Hour != timePickerDate.Hour
+                || datePickerDate.Minute != timePickerDate.Minute
+                || datePickerDate.Second != timePickerDate.Second)
+            {
+                var result = new DateTime(datePickerDate.Year,
+                     datePickerDate.Month,
+                     datePickerDate.Day,
+                     timePickerDate.Hour,
+                     timePickerDate.Minute,
+                     timePickerDate.Second);
+
+                return result;
+            }
+
+            return datePickerDate;
+        }
+    }
+}
